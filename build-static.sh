4e61b4c1e3e39397958602a927fafe061f3cd8f7
Ian Reynolds <idreyn@gmail.com>
2023-06-02 20:02:47 -0400

    try now

--- build-static.sh
+++ build-static.sh
@@ -1,5 +1,6 @@
 export DOMAIN="idreyn.github.io/utms-reader/"
 export ASSET_PREFIX="https://$DOMAIN"
+export BASE_PATH="/utms-reader"
 
 rm -r ./docs
 npx next build
--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@
 const nextConfig = {
     output: "export",
     assetPrefix: process.env.ASSET_PREFIX,
+    basePath: process.env.BASE_PATH,
 };
+
+console.log({ nextConfig });
 
 module.exports = nextConfig;
