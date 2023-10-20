4df51bb870ae8d0dc0eb92de63d0054104fb35a4
Ian Reynolds <idreyn@gmail.com>
2023-10-20 17:18:57 -0400

    Build: Fri Oct 20 17:18:57 EDT 2023

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,30 +16,32 @@ const info = (
         </p>
         <p>
             Like everyone else at Shanghai's top university, Mona Xu is a
-            neikonaut: mind soaked with powerful psychedelics and intertwined
-            with a supercomputer, she performs calculations she can barely
-            imagine sober. Most neikonauts return from their trips with
-            theorems, reactor designs, or millions in parallel yuan. Mona is a
-            clinician, and she's just lucky to be here. She mops up after
+            neikonaut: mind soaked with psychedelics and intertwined with a
+            supercomputer, she performs calculations she can barely imagine
+            sober. Most neikonauts return from their trips with theorems,
+            reactor designs, or millions in parallel yuan. Mona is a clinician,
+            and she's just lucky to be here, never quite living up to the
+            expectations of her illustrious advisor, Dr. Deng. She mops up after
             everyone else's recklessness, tunneling into their minds and
             dredging out the digital debris that gets left behind.
         </p>
         <p>
-            The most exciting thing in Mona's life was her flatmate, Cai Yuhui.
-            Cai taught her to probe Shanghai's bizarrely anonymized surveillance
-            system, to chase the Ripples inhabiting it towards strange,
-            electrifying, nocturnal dimensions of self and city. She hurt
-            herself badly that way. Her advisor is sympathetic, in her own
-            stilted way: urging Mona to heal, forget, and cobble together a
-            dissertation.
+            And once, Mona Xu was a Ripplechaser. Shanghai's billion anonymizing
+            CCTV cameras reveal the Mirror Sea: a strange, electrifying,
+            nocturnal dimension of self and city inhabited by primal creatures
+            called Ripples, their motion borrowed from our own. Like many
+            others, Mona has a hard time looking away — even after it triggers a
+            psychotic break. Dr. Deng begs her to move on, write a dissertation,
+            and graduate. But Mona is about to discover something in a patient's
+            mind that pushes her back to that boundary between reality and
+            delusion, observer and observed. Back to the Mirror Sea. The Ripples
+            have something they shouldn't, and Dr. Deng knows something she
+            won't say.
         </p>
         <p>
-            But she's about to discover something in a patient's mind that
-            pushes her back to that boundary between reality and delusion,
-            observer and observed. Back to the Mirror Sea. On the eve of crisis,
-            against the wishes of her advisor, Mona must guide an
-            earth-shattering revelation into the world: when we are together,{" "}
-            <i>we are not alone</i>.
+            On the eve of crisis, against the wishes of her advisor, Mona must
+            guide a dangerous revelation safely into the world: when we are
+            together, <i>we are not alone</i>.
         </p>
     </>
 );
