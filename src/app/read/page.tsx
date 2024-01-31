1a6810092f1cc551eb86274ae2ae0232ba52f2e7
Ian Reynolds <idreyn@gmail.com>
2024-01-30 20:51:08 -0500

    Build: Tue Jan 30 20:51:08 EST 2024

--- src/app/read/page.tsx
+++ src/app/read/page.tsx
@@ -3,14 +3,7 @@ import React from "react";
 import { loadManuscriptJson } from "@/ingest";
 import { TableOfContents } from "@/components";
 
-const teaser = (
-    <div style={{ color: "var(--color-incomplete)" }}>
-        <h2>Part 5 of 5</h2>
-        <i>Coming soon</i>
-    </div>
-);
-
 export default function TocPage() {
     const manuscript = loadManuscriptJson();
-    return <TableOfContents manuscript={manuscript} teaser={teaser} />;
+    return <TableOfContents manuscript={manuscript} />;
 }
